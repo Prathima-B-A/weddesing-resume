7013562a20d84254014c7b76364a6e6caa8f30ac
Prathima-B-A <155656243+Prathima-B-A@users.noreply.github.com>
2024-01-06 10:36:50 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,4 @@
+function gotoLink(link){
+    console.log(link);
+    location.href='https://www.linkedin.com/posts/prathima-b-a-5a50702a7_a-study-of-air-quality-index-activity-7146852268074496000-1fxl?utm_source=share&utm_medium=member_android'
+};
